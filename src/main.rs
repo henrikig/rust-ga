931cf74a9c55f3cead1d3c1ff3c9cce62b707820
jorgenweidemann <jor.weidemann@gmail.com>
2022-03-04 14:41:40 +0100

    Started parser

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+extern crate serde;
+extern crate serde_json;
+use serde_json::Value as JsonValue;
+#[macro_use]
+extern crate serde_derive;
+
+struct Instance {
+    products: u32
+    stages: u32
+    machines: Vec<u64>
+    production_times: Vec<Vec<u64>>
+    setup_times: Vec<Vec<u64>>
+}
+
 fn main() {
     println!("Hello, world!");
 }
