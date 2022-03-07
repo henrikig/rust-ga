fa502ef5ab904f39597ce62b1e30556d7458ccb4
Henrik Irgens Gravdal <henrikigravdal@gmail.com>
2022-03-07 15:33:08 +0100

    Clean up main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,7 @@
 mod common;
 mod genetic_algorithm;
-use genetic_algorithm::{ga::GA, params::PROBLEM_FILE};
+
+use genetic_algorithm::ga;
 fn main() {
-    let mut ga = GA::new(PROBLEM_FILE);
-    ga.run()
+    ga::run();
 }
-
-/*
-mod genetic_algorithm;
-
-use genetic_algorithm::{ga::GA, params::PROBLEM_FILE};
-@@ -6,3 +11,5 @@ fn main() {
-    let mut ga = GA::new(PROBLEM_FILE);
-    ga.run();
-}
-*/
