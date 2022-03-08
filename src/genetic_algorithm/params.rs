eb243f89f478ab308c052c8ccb1f7b65cdae1962
Henrik Irgens Gravdal <henrikigravdal@gmail.com>
2022-03-08 19:25:12 +0100

    Add new crossover operator

--- src/genetic_algorithm/params.rs
+++ src/genetic_algorithm/params.rs
@@ -1,7 +1,10 @@
+use super::operators::crossover::XTYPE;
+
 pub const PROBLEM_FILE: &str = "./instances/ruiz/json/n20m2-1.json";
 pub const POPULATION_SIZE: usize = 100;
 pub const ITERATIONS: usize = 50000;
 pub const ELITISM: usize = 2;
 pub const KEEP_BEST: f32 = 0.8;
 pub const XOVER_PROB: f32 = 0.5;
+pub const XOVER: XTYPE = XTYPE::BCBC;
 pub const MUTATION_PROB: f32 = 0.05;
