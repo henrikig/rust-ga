367ebad18bf3a3bcd792444d1ae4414cbd48875f
Henrik Irgens Gravdal <henrikigravdal@gmail.com>
2022-03-08 09:52:59 +0100

    Add steady-state version of GA

--- src/genetic_algorithm/params.rs
+++ src/genetic_algorithm/params.rs
@@ -1,6 +1,6 @@
 pub const PROBLEM_FILE: &str = "./instances/ruiz/json/n20m2-1.json";
 pub const POPULATION_SIZE: usize = 100;
-pub const ITERATIONS: usize = 200;
+pub const ITERATIONS: usize = 50000;
 pub const ELITISM: usize = 2;
 pub const KEEP_BEST: f32 = 0.8;
 pub const XOVER_PROB: f32 = 0.5;
