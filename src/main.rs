86db95cfad3607f8258959f2b08df593800dd67e
jorgenweidemann <jor.weidemann@gmail.com>
2022-03-14 13:38:23 +0100

    Resolved last merge conflicting file

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,7 @@
 mod common;
-
-use crate::common::common_tests;
-fn main() {
-    println!("Hello!");
-}
-
-/*
 mod genetic_algorithm;
 
-use genetic_algorithm::{ga::GA, params::PROBLEM_FILE};
-@@ -6,3 +11,5 @@ fn main() {
-    let mut ga = GA::new(PROBLEM_FILE);
-    ga.run();
+use genetic_algorithm::ga;
+fn main() {
+    ga::run();
 }
-*/
