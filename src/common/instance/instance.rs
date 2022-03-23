8b95a2a008eabc8d1fd09e7565eedbba867fa50b
jorgenweidemann <jor.weidemann@gmail.com>
2022-03-23 13:21:08 +0100

    Changed description of instance

--- src/common/instance/instance.rs
+++ src/common/instance/instance.rs
@@ -3,7 +3,7 @@ use serde_derive::Deserialize;
 /*
 machines[stage]
 processing_times[job][stage]
-setup_times:[machine][previous_job][current_job]
+setup_times:[stage][previous_job][current_job]
 Note: When previous job and current is the same, it is the inital setup time
 */
 
@@ -14,4 +14,12 @@ pub struct Instance {
     pub machines: Vec<u32>,
     pub processing_times: Vec<Vec<u32>>,
     pub setup_times: Vec<Vec<Vec<u32>>>,
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn testing() {
+        print!("yes")
+    }
 }
