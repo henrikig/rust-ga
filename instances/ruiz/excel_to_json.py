051bc9d40feb1eaefe8a022f83c5f48b4334127b
jorgenweidemann <jor.weidemann@gmail.com>
2022-03-24 10:42:34 +0100

    Converted all instances to .json

--- instances/ruiz/excel_to_json.py
+++ instances/ruiz/excel_to_json.py
@@ -21,6 +21,7 @@ def split_dataframe(df: pd.DataFrame, height: int, width: int, n: int) -> list:
         df_list.append(df_keep)
     if not df.empty:
         df_list.append(df)
+    
     return df_list
 
 # Tests if all but first entry in the first row is zero values
@@ -99,8 +100,104 @@ n80m4_constant = split_dataframe(df=df, height=height, width=width, n=40)
 df = pd.read_excel(io="n=80\\n=80,m=4\\Variable\\n=80, m=4, Var.xls", header=None)
 n80m4_variable = split_dataframe(df=df, height=height, width=width, n=40)
 
+df = pd.read_excel(io="n=80\\n=80,m=8\\SDST25\\n=80, m=8, 25_.xls", header=None)
+height = 723
+n80m8_25 = split_dataframe(df=df, height=height, width=width, n=20)
+
+df = pd.read_excel(io="n=80\\n=80,m=8\\SDST50\\n=80, m=4, 50_.xls", header=None)
+n80m8_50 = split_dataframe(df=df, height=height, width=width, n=20)
+
+df = pd.read_excel(io="n=80\\n=80,m=8\\SDST100\\n=80, m=4, 100_.xls", header=None)
+n80m8_100 = split_dataframe(df=df, height=height, width=width, n=20)
+
+df = pd.read_excel(io="n=80\\n=80,m=8\\SDST125\\n=80, m=4, 125_.xls", header=None)
+n80m8_125 = split_dataframe(df=df, height=height, width=width, n=20)
+
+width = 120
+
+df = pd.read_excel(io="n=120\\n=120,m=2\\Constant\\n=120, m=2, Con.xls", header=None)
+height = 363
+n120m2_constant = split_dataframe(df=df, height=height, width=width, n=40)
+
+df = pd.read_excel(io="n=120\\n=120,m=2\\Variable\\n=120, m=2, Var.xls", header=None)
+n120m2_variable = split_dataframe(df=df, height=height, width=width, n=40)
+
+df = pd.read_excel(io="n=120\\n=120,m=4\\SDST25\\n=120, m=4, 25_.xls", header=None)
+height = 603
+n120m4_25 = split_dataframe(df=df, height=height, width=width, n=20)
+
+df = pd.read_excel(io="n=120\\n=120,m=4\\SDST50\\n=120, m=4, 50_.xls", header=None)
+n120m4_50 = split_dataframe(df=df, height=height, width=width, n=20)
+
+df = pd.read_excel(io="n=120\\n=120,m=4\\SDST100\\n=120, m=4, 100_.xls", header=None)
+n120m4_100 = split_dataframe(df=df, height=height, width=width, n=20)
+
+df = pd.read_excel(io="n=120\\n=120,m=4\\SDST125\\n=120, m=4, 125_.xls", header=None)
+n120m4_125 = split_dataframe(df=df, height=height, width=width, n=20)
+
+df = pd.read_excel(io="n=120\\n=120,m=8\\SDST25\\Constant\\n = 120, m = 8, 25_, Con.xls", header=None)
+height = 1083
+n120m8_25_constant = split_dataframe(df=df, height=height, width=width, n=10)
+
+df = pd.read_excel(io="n=120\\n=120,m=8\\SDST25\\Variable\\n=120, m = 8, 25_, Var.xls", header=None)
+n120m8_25_variable = split_dataframe(df=df, height=height, width=width, n=10)
+
+df = pd.read_excel(io="n=120\\n=120,m=8\\SDST50\\Constant\\n=120, m=5, 50_, Con.xls", header=None) # Believe the m5 should have been m8
+n120m8_50_constant = split_dataframe(df=df, height=height, width=width, n=10)
+
+df = pd.read_excel(io="n=120\\n=120,m=8\\SDST50\\Variable\\n=120, m=8, 50_, Var.xls", header=None)
+n120m8_50_variable = split_dataframe(df=df, height=height, width=width, n=10)
+
+df = pd.read_excel(io="n=120\\n=120,m=8\\SDST100\\Constant\\n=120, m=8, 100_, Con.xls", header=None)
+n120m8_100_constant = split_dataframe(df=df, height=height, width=width, n=10)
+
+df = pd.read_excel(io="n=120\\n=120,m=8\\SDST100\\Variable\\n=120, m=8, 100_, Var.xls", header=None)
+n120m8_100_variable = split_dataframe(df=df, height=height, width=width, n=10)
+
+df = pd.read_excel(io="n=120\\n=120,m=8\\SDST125\\Constant\\n=120, m=8, 125_, Con.xls", header=None)
+n120m8_125_constant = split_dataframe(df=df, height=height, width=width, n=10)
+
+df = pd.read_excel(io="n=120\\n=120,m=8\\SDST125\\Variable\\n=120, m=8, 125_, Var.xls", header=None)
+n120m8_125_variable = split_dataframe(df=df, height=height, width=width, n=10)
+
 
 # Conversion of dataframes to .json
 
 for index in range(80):
     to_json(df=n20m2[index], save_as=f"n20m2-{index+1}")
+    to_json(df=n20m4[index], save_as=f"n20m4-{index+1}")
+    to_json(df=n20m8[index], save_as=f"n20m8-{index+1}")
+    to_json(df=n50m2[index], save_as=f"n50m2-{index+1}")
+    to_json(df=n50m4[index], save_as=f"n50m4-{index+1}")
+    to_json(df=n50m8[index], save_as=f"n50m8-{index+1}")
+    to_json(df=n80m2[index], save_as=f"n80m2-{index+1}")
+
+for index in range(40):
+    to_json(df=n80m4_constant[index], save_as=f"n80m4-{index+1}")
+    to_json(df=n80m4_variable[index], save_as=f"n80m4-{index+41}")
+
+for index in range(20):
+    to_json(df=n80m8_25[index], save_as=f"n80m8-{index+1}")
+    to_json(df=n80m8_50[index], save_as=f"n80m8-{index+21}")
+    to_json(df=n80m8_100[index], save_as=f"n80m8-{index+41}")
+    to_json(df=n80m8_125[index], save_as=f"n80m8-{index+61}")
+
+for index in range(40):
+    to_json(df=n120m2_constant[index], save_as=f"n120m2-{index+1}")
+    to_json(df=n120m2_variable[index], save_as=f"n120m2-{index+41}")
+
+for index in range(20):
+    to_json(df=n120m4_25[index], save_as=f"n120m4-{index+1}")
+    to_json(df=n120m4_50[index], save_as=f"n120m4-{index+21}")
+    to_json(df=n120m4_100[index], save_as=f"n120m4-{index+41}")
+    to_json(df=n120m4_125[index], save_as=f"n120m4-{index+61}")
+
+for index in range(10):
+    to_json(df=n120m8_25_constant[index], save_as=f"n120m8-{index+1}")
+    to_json(df=n120m8_25_variable[index], save_as=f"n120m8-{index+11}")
+    to_json(df=n120m8_50_constant[index], save_as=f"n120m8-{index+21}")
+    to_json(df=n120m8_50_variable[index], save_as=f"n120m8-{index+31}")
+    to_json(df=n120m8_100_constant[index], save_as=f"n120m8-{index+41}")
+    to_json(df=n120m8_100_variable[index], save_as=f"n120m8-{index+51}")
+    to_json(df=n120m8_125_constant[index], save_as=f"n120m8-{index+61}")
+    to_json(df=n120m8_125_variable[index], save_as=f"n120m8-{index+71}")
