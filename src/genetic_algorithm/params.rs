7486b9eb33430d333606e56e89ffabe6dab9b61f
Henrik Irgens Gravdal <henrikigravdal@gmail.com>
2022-05-18 11:00:09 +0200

    Setup for local search

--- src/genetic_algorithm/params.rs
+++ src/genetic_algorithm/params.rs
@@ -9,12 +9,12 @@ pub const WRITE_IMPROVEMENT: bool = false;
 pub const POPULATION_SIZE: usize = 150;
 pub const ITERATIONS: usize = 180;
 pub const ELITISM: usize = 2;
-pub const LOCAL_SEARCH: bool = false;
+pub const LOCAL_SEARCH: bool = true;
 pub const KEEP_BEST: f32 = 0.8;
 pub const K_TOURNAMENT: usize = 2;
 pub const XOVER_PROB: f32 = 0.5;
-pub const XOVER: XTYPE = XTYPE::PMX;
-pub const CONSTRUCTION: Construction = Construction::MDDR(0.2);
+pub const XOVER: XTYPE = XTYPE::QLearning;
+pub const CONSTRUCTION: Construction = Construction::MDDR(1.0);
 pub const NON_IMPROVING_ITERATIONS: usize = 100; // use 50, 100, 150 (because of implementation)
 pub const RTYPE: RTYPE = RTYPE::GCH;
 pub const ALLWAYS_KEEP: f64 = 1.0; // Percentage of population to always keep
@@ -25,8 +25,8 @@ pub const LEARNING_RATE: f64 = 0.2;
 pub const EPSILON: f64 = 0.25;
 
 // MUTATION
-pub const MUTATION_PROB: f32 = 0.0;
-pub const MTYPE: MTYPE = MTYPE::Shift;
+pub const MUTATION_PROB: f32 = 0.1;
+pub const MTYPE: MTYPE = MTYPE::Random;
 pub const REVERSAL_PERCENT: usize = 10;
 
 // CROWDING
@@ -35,8 +35,8 @@ pub const CROWDING_SCALE: f64 = 0.5;
 pub const K_NEAREST: usize = 10; // Only used in steady state crowding version
 pub const DISTANCE_METRIC: DTYPE = DTYPE::ExactMatch;
 
-pub const IG_GRID_SEARCH: bool = true;
+pub const IG_GRID_SEARCH: bool = false;
 
 // Solution folder for parameter grid search
 
-pub const SOLUTION_FOLDER: &str = "./solutions/mutation-prob-02";
+pub const SOLUTION_FOLDER: &str = "./solutions/local_search";
